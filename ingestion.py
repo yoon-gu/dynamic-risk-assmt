15063f5f75c356cbdc8685f232d378d2ddd2095f
Yoon-gu Hwang <yz0624@gmail.com>
2022-11-18 01:59:25 +0900

    step 1

--- ingestion.py
+++ ingestion.py
@@ -9,18 +9,27 @@ from datetime import datetime
 
 #############Load config.json and get input and output paths
 with open('config.json','r') as f:
-    config = json.load(f) 
+    config = json.load(f)
 
 input_folder_path = config['input_folder_path']
 output_folder_path = config['output_folder_path']
 
 
-
 #############Function for data ingestion
 def merge_multiple_dataframe():
-    #check for datasets, compile them together, and write to an output file
+    filenames = os.listdir(os.path.join(input_folder_path))
+    df_list = pd.DataFrame()
+    record_file_path = os.path.join(output_folder_path, 'ingestedfiles.txt')
+    with open(record_file_path, 'w') as record_file:
+        for each_filename in filenames:
+            input_file_path = os.path.join(input_folder_path, each_filename)
+            df = pd.read_csv(input_file_path)
+            df_list = pd.concat([df_list, df])
 
+            record_file.write(each_filename + '\n')
 
+    result = df_list.drop_duplicates()
+    result.to_csv(os.path.join(output_folder_path, 'finaldata.csv'), index=False)
 
 if __name__ == '__main__':
     merge_multiple_dataframe()
