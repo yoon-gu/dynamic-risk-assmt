222d94655ae7a5a451edb37bff45121fb1102def
Yoon-gu Hwang <yz0624@gmail.com>
2022-11-18 11:22:45 +0900

    add scoring

--- scoring.py
+++ scoring.py
@@ -12,14 +12,30 @@ import json
 
 #################Load config.json and get path variables
 with open('config.json','r') as f:
-    config = json.load(f) 
+    config = json.load(f)
 
-dataset_csv_path = os.path.join(config['output_folder_path']) 
-test_data_path = os.path.join(config['test_data_path']) 
+dataset_csv_path = os.path.join(config['output_folder_path'])
+test_data_path = os.path.join(config['test_data_path'])
 
 
 #################Function for model scoring
 def score_model():
     #this function should take a trained model, load test data, and calculate an F1 score for the model relative to the test data
     #it should write the result to the latestscore.txt file
+    model_path = os.path.join(config['output_model_path'])
+    model_file_path = os.path.join(model_path, 'trainedmodel.pkl')
+    with open(model_file_path, 'rb') as f:
+        model = pickle.load(f)
 
+    test_df = pd.read_csv(os.path.join(test_data_path, 'testdata.csv'))
+    X = test_df[['lastmonth_activity', 'lastyear_activity', 'number_of_employees']].values.reshape(-1, 3)
+    y = test_df['exited'].values.reshape(-1, 1)
+
+    predicted = model.predict(X)
+    f1score = metrics.f1_score(predicted, y)
+    latest_score_file = os.path.join(model_path, 'latestscore.txt')
+    with open(latest_score_file, 'w') as f:
+        f.write(str(f1score))
+
+if __name__ == '__main__':
+    score_model()
