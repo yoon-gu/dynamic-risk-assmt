6ecd633d52e934aa71b0139fa2ed7ca15806fd67
Yoon-gu Hwang <yz0624@gmail.com>
2022-11-18 15:22:23 +0900

    api calls and app.py

--- apicalls.py
+++ apicalls.py
@@ -1,20 +1,23 @@
 import requests
+import json
+import os
 
 #Specify a URL that resolves to your workspace
-URL = "http://127.0.0.1/"
-
-
+URL = "http://127.0.0.1:8000/"
 
 #Call each API endpoint and store the responses
-response1 = #put an API call here
-response2 = #put an API call here
-response3 = #put an API call here
-response4 = #put an API call here
+response1 = requests.post(URL+'/prediction?inputdata=testdata/testdata.csv').content
+response2 = requests.get(URL+'/scoring').content
+response3 = requests.get(URL+'/summarystats').content
+response4 = requests.get(URL+'/diagnostics').content
 
 #combine all API responses
-responses = #combine reponses here
+responses = [response1, response2, response3, response4]
 
 #write the responses to your workspace
+with open('config.json','r') as f:
+    config = json.load(f) 
 
-
-
+apireturn_path = os.path.join(config['output_model_path'], 'apireturns.txt')
+with open(apireturn_path, 'w') as file:
+    file.write(str(responses))
--- app.py
+++ app.py
@@ -2,11 +2,11 @@ from flask import Flask, session, jsonify, request
 import pandas as pd
 import numpy as np
 import pickle
-import create_prediction_model
-import diagnosis
-import predict_exited_from_saved_model
 import json
 import os
+
+from diagnostics import model_predictions, dataframe_summary, execution_time, investigation_missing_data, outdated_packages_list
+from scoring import score_model
 
 
 
@@ -19,32 +19,47 @@ with open('config.json','r') as f:
 
 dataset_csv_path = os.path.join(config['output_folder_path'])
 
-prediction_model = None
+with open(os.path.join( config['prod_deployment_path'], "trainedmodel.pkl"), "rb") as f:
+    prediction_model = pickle.load(f)
 
 
 #######################Prediction Endpoint
 @app.route("/prediction", methods=['POST','OPTIONS'])
 def predict():
     #call the prediction function you created in Step 3
-    return #add return value for prediction outputs
+    filename = request.args.get('inputdata')
+    if not os.path.isfile(filename):
+        return f"{filename} doesn't exist"
+    pred = model_predictions(filename)
+    return str(pred)
 
 #######################Scoring Endpoint
 @app.route("/scoring", methods=['GET','OPTIONS'])
-def stats():
+def scoring():
     #check the score of the deployed model
-    return #add return value (a single F1 score number)
+    f1score = score_model()
+    return str(f1score)
 
 #######################Summary Statistics Endpoint
 @app.route("/summarystats", methods=['GET','OPTIONS'])
 def stats():
     #check means, medians, and modes for each column
-    return #return a list of all calculated summary statistics
+    statistics = dataframe_summary()
+    return str(statistics)
 
 #######################Diagnostics Endpoint
 @app.route("/diagnostics", methods=['GET','OPTIONS'])
-def stats():
+def diagnostics():
     #check timing and percent NA values
-    return #add return value for all diagnostics
+    timing = execution_time()
+    missing_data = investigation_missing_data()
+    dependencies = outdated_packages_list()
+    res =   {
+                'timing' : timing,
+                'missing_data' : missing_data,
+                'dependency_check' : dependencies,
+            }
+    return str(res)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000, debug=True, threaded=True)
